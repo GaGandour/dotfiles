273bef6e13aa401e3d32b36091203b8818bfcf52
Gabriel Gandour <gagandour@gmail.com>
2024-07-11 12:00:21 -0300

    change flash to hop

--- .config/nvim/lua/gandour/plugins/flash.lua
+++ .config/nvim/lua/gandour/plugins/flash.lua
@@ -1,11 +1,20 @@
 return {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-        { "<leader>l", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-        { "<leader>L", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- "folke/flash.nvim",
+    -- event = "VeryLazy",
+    -- ---@type Flash.Config
+    -- opts = {},
+    -- -- stylua: ignore
+    -- keys = {
+    --     { "<leader>l", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+    --     { "<leader>L", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- },
+    "smoka7/hop.nvim",
+    version = "*",
+    opts = {
+        keys = 'etovxqpdygfblzhckisuran',
     },
+    keys = {
+        { "<leader>l", mode = { "n", "x", "o" }, "<CMD>HopChar1MW<CR>", desc = "Hop to character" },
+        { "<leader>L", mode = { "n", "x", "o" }, "<CMD>HopPatternMW<CR>", desc = "Hop to pattern" },
+    }
 }
