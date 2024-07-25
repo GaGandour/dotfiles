2ec6d4228f9cc6a8a4f99b0c8a0cc71423847ec5
Gabriel Gandour <gagandour@gmail.com>
2024-07-25 14:13:04 -0300

    add clojure to nvim

--- .config/nvim/lua/gandour/plugins/conjure.lua
+++ .config/nvim/lua/gandour/plugins/conjure.lua
@@ -1,2 +1,30 @@
 -- This is to work with clojure
-return { "Olical/conjure" }
+return {
+	{
+		"Olical/conjure",
+		ft = { "clojure", "fennel", "python" }, -- etc
+		lazy = true,
+		init = function()
+			-- Set configuration options here
+			-- vim.g["conjure#debug"] = true
+			vim.g["conjure#mapping#prefix"] = ','
+		end,
+
+		-- Optional cmp-conjure integration
+		dependencies = { "PaterJason/cmp-conjure" },
+	},
+	{
+		"PaterJason/cmp-conjure",
+		lazy = true,
+		config = function()
+			local cmp = require("cmp")
+			local config = cmp.get_config()
+			table.insert(config.sources, { name = "conjure" })
+			return cmp.setup(config)
+		end,
+	},
+	{ "Olical/aniseed" },
+	{ "tpope/vim-dispatch" },
+	{ "clojure-vim/vim-jack-in" },
+	{ "radenling/vim-dispatch-neovim" },
+}
