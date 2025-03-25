7cd9b5b8149a2490691dc4abaec9b91ad83e3d31
Gabriel Gandour <gagandour@gmail.com>
2025-03-25 13:50:06 -0300

    add yazi to config!

--- .config/nvim/lua/gandour/plugins/auto-session.lua
+++ .config/nvim/lua/gandour/plugins/auto-session.lua
@@ -8,9 +8,9 @@ return {
             auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
         })
 
-        local keymap = vim.keymap
+        -- local keymap = vim.keymap
 
-        keymap.set("n", "<leader>yr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })             -- restore last workspace session for current directory
-        keymap.set("n", "<leader>ys", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+        -- keymap.set("n", "<leader>yr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })             -- restore last workspace session for current directory
+        -- keymap.set("n", "<leader>ys", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
     end,
 }
