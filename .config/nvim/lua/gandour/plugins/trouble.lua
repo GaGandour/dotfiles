b117601893ed2a0e3fdc71684716ca4f43a2c64a
Gabriel Gandour <gagandour@gmail.com>
2025-04-10 09:53:29 -0300

    revert trouble in snack

--- .config/nvim/lua/gandour/plugins/trouble.lua
+++ .config/nvim/lua/gandour/plugins/trouble.lua
@@ -1,25 +1,12 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-	optional = true,
-	specs = {
-		"folke/snacks.nvim",
-		opts = function(_, opts)
-			return vim.tbl_deep_extend("force", opts or {}, {
-				picker = {
-					actions = require("trouble.sources.snacks").actions,
-					win = {
-						input = {
-							keys = {
-								["<leader>xx"] = {
-									"trouble_open",
-									mode = { "n", "i" },
-								},
-							},
-						},
-					},
-				},
-			})
-		end,
+	keys = {
+		{ "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
+		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
+		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
+		{ "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
+		{ "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
+		{ "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
 	},
 }
