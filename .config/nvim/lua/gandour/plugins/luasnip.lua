6f1e6cf59561fe4f16f14c5f447de99fafcaf589
Gabriel Gandour <gagandour@gmail.com>
2025-04-10 11:25:14 -0300

    first try

---
+++ .config/nvim/lua/gandour/plugins/luasnip.lua
@@ -0,0 +1,109 @@
+-- Filename: ~/github/dotfiles-latest/neovim/neobean/lua/plugins/luasnip.lua
+-- ~/github/dotfiles-latest/neovim/neobean/lua/plugins/luasnip.lua
+
+-- This allows me to create my custom snippets
+-- All you need to do, if using the lazyvim.org distro, is to enable the
+-- coding.luasnip LazyExtra and then add this file
+
+-- If you're a dotfiles scavenger, definitely watch this video (you're welcome)
+-- https://youtu.be/FmHhonPjvvA?si=8NrcRWu4GGdmTzee
+
+return {
+  "L3MON4D3/LuaSnip",
+  enabled = true,
+  opts = function(_, opts)
+    local ls = require("luasnip")
+
+    -- Add prefix ";" to each one of my snippets using the extend_decorator
+    -- I use this in combination with blink.cmp. This way I don't have to use
+    -- the transform_items function in blink.cmp that removes the ";" at the
+    -- beginning of each snippet. I added this because snippets that start with
+    -- a symbol like ```bash aren't having their ";" removed
+    -- https://github.com/L3MON4D3/LuaSnip/discussions/895
+    -- NOTE: THis extend_decorator works great, but I also tried to add the ";"
+    -- prefix to all of the snippets loaded from friendly-snippets, but I was
+    -- unable to do so, so I still have to use the transform_items in blink.cmp
+    local extend_decorator = require("luasnip.util.extend_decorator")
+    -- Create trigger transformation function
+    local function auto_semicolon(context)
+      if type(context) == "string" then
+        return { trig = ";" .. context }
+      end
+      return vim.tbl_extend("keep", { trig = ";" .. context.trig }, context)
+    end
+    -- Register and apply decorator properly
+    extend_decorator.register(ls.s, {
+      arg_indx = 1,
+      extend = function(original)
+        return auto_semicolon(original)
+      end,
+    })
+    local s = extend_decorator.apply(ls.s, {})
+
+    -- local s = ls.snippet
+    local t = ls.text_node
+    local i = ls.insert_node
+    local f = ls.function_node
+
+    local function clipboard()
+      return vim.fn.getreg("+")
+    end
+
+    -- Check if the file exists before proceeding
+    -- Custom snippets
+    -- the "all" after ls.add_snippets("all" is the filetype, you can know a
+    -- file filetype with :set ft
+    -- Custom snippets
+
+    -- #####################################################################
+    --                            Markdown
+    -- #####################################################################
+
+    -- Helper function to create code block snippets
+    local function create_code_block_snippet(lang)
+      return s({
+        trig = lang,
+        name = "Codeblock",
+        desc = lang .. " codeblock",
+      }, {
+        t({ "```" .. lang, "" }),
+        i(1),
+        t({ "", "```" }),
+      })
+    end
+
+    -- Define languages for code blocks
+    local languages = {
+      "txt",
+      "lua",
+      "sql",
+      "go",
+      "regex",
+      "bash",
+      "markdown",
+      "markdown_inline",
+      "yaml",
+      "json",
+      "jsonc",
+      "cpp",
+      "csv",
+      "java",
+      "javascript",
+      "python",
+      "dockerfile",
+      "html",
+      "css",
+      "templ",
+      "php",
+    }
+
+    -- Generate snippets for all languages
+    local snippets = {}
+
+    for _, lang in ipairs(languages) do
+      table.insert(snippets, create_code_block_snippet(lang))
+    end
+
+    return opts
+  end,
+}
