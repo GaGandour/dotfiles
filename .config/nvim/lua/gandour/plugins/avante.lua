ef2cd8dca75aff0f4bb17fa9318c37941c5f34be
Gabriel Gandour <gagandour@gmail.com>
2025-04-11 17:10:53 -0300

    set avante border to rounded

--- .config/nvim/lua/gandour/plugins/avante.lua
+++ .config/nvim/lua/gandour/plugins/avante.lua
@@ -1,27 +1,30 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "copilot",
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	opts = {
+		-- add any opts here
+		-- for example
+		provider = "copilot",
+		windows = { ask = { start_insert = false }, edit = { border = "rounded" } },
+        -- Maps
+        vim.keymap.set("n", "<leader>ac", ":AvanteChat<CR>")
+	},
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
 }
