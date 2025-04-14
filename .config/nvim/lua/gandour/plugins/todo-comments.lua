56866d23b7e900fcf1dcf749864bc8be123c4e32
Gabriel Gandour <gagandour@gmail.com>
2025-04-14 20:54:38 -0300

    lint todo comments

--- .config/nvim/lua/gandour/plugins/todo-comments.lua
+++ .config/nvim/lua/gandour/plugins/todo-comments.lua
@@ -1,8 +1,20 @@
 return {
-  "folke/todo-comments.nvim",
-  optional = true,
-  keys = {
-    { "<leader>ft", function() Snacks.picker.todo_comments() end, desc = "Todo" },
-    { "<leader>fT", function () Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } }) end, desc = "Todo/Fix/Fixme" },
-  },
+	"folke/todo-comments.nvim",
+	optional = true,
+	keys = {
+		{
+			"<leader>ft",
+			function()
+				Snacks.picker.todo_comments()
+			end,
+			desc = "Todo",
+		},
+		{
+			"<leader>fT",
+			function()
+				Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+			end,
+			desc = "Todo/Fix/Fixme",
+		},
+	},
 }
