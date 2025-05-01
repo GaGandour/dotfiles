e0940ff8dcfcd93876fbe8d105f1a38c36935a96
Gabriel Gandour <gagandour@gmail.com>
2025-05-01 18:00:19 -0300

    wrap md

--- .config/nvim/lua/gandour/core/options.lua
+++ .config/nvim/lua/gandour/core/options.lua
@@ -7,34 +7,34 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 4       -- 4 spaces for tabs (prettier default)
-opt.shiftwidth = 4    -- 4 spaces for indent width
-opt.expandtab = true  -- expand tab to spaces
+opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
-vim.api.nvim_create_autocmd('BufEnter', {
-    pattern = {  '*.tex', '*.txt', '*.norg' },
-    command = 'setlocal wrap'
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = { "*.md", "*.tex", "*.txt", "*.norg" },
+	command = "setlocal wrap",
 })
 
 -- This is for obsidian (markdown) concealer
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = {"*.md"},
-  command = "set conceallevel=2"
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.md" },
+	command = "set conceallevel=2",
 })
 
 -- This is for neorg concealer
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = {"*.norg"},
-  command = "set conceallevel=3"
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.norg" },
+	command = "set conceallevel=3",
 })
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -69,10 +69,10 @@ opt.scrolloff = 10
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
